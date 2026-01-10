7175b5bf67c0d07d4d7c0e54c5ac44d37371432e
Mohamud Adam <mohamud.tech@gmail.com>
2026-01-10 00:06:36 -0600

    Update setupRedis.ts

--- module/setupRedis.ts
+++ module/setupRedis.ts
@@ -1,12 +1,12 @@
 import { Config } from "@pulumi/pulumi";
 import { pulumi } from "@vizientinc/pulumi";
 import * as cache from "@pulumi/azure-native/cache/v20241001";
+import * as network from "@pulumi/azure-native/network";
 import { AzureBuilder } from "@vizientinc/azure-builder";
 
 export interface RedisEnterpriseConfig {
-  skuName: "Balanced_B0" | "Balanced_B1" | "Balanced_B3" | "Balanced_B5" | "Balanced_B10" | "Balanced_B20" | "Balanced_B50" | "Balanced_B100" | "Balanced_B150" | "Balanced_B250" | "Balanced_B350" | "Balanced_B500" | "Balanced_B700" | "Balanced_B1000" | "MemoryOptimized_M10" | "MemoryOptimized_M20" | "MemoryOptimized_M50" | "MemoryOptimized_M100" | "MemoryOptimized_M150" | "MemoryOptimized_M250" | "MemoryOptimized_M350" | "MemoryOptimized_M500" | "MemoryOptimized_M700" | "MemoryOptimized_M1000" | "MemoryOptimized_M1500" | "MemoryOptimized_M2000" | "ComputeOptimized_X3" | "ComputeOptimized_X5" | "ComputeOptimized_X10" | "ComputeOptimized_X20" | "ComputeOptimized_X50" | "ComputeOptimized_X100" | "ComputeOptimized_X150" | "ComputeOptimized_X250" | "ComputeOptimized_X350" | "ComputeOptimized_X500" | "FlashOptimized_A250" | "FlashOptimized_A500" | "FlashOptimized_A700" | "FlashOptimized_A1000" | "FlashOptimized_A1500" | "FlashOptimized_A2000" | "FlashOptimized_A4500";
-  minimumTlsVersion?: "1.0" | "1.1" | "1.2";
-  zones?: string[];
+  skuName: "Enterprise_E10" | "Enterprise_E20" | "Enterprise_E50" | "Enterprise_E100" | "EnterpriseFlash_F300" | "EnterpriseFlash_F700" | "EnterpriseFlash_F1500";
+  capacity: number;
   clusteringPolicy?: "EnterpriseCluster" | "OSSCluster";
   evictionPolicy?: "AllKeysLFU" | "AllKeysLRU" | "AllKeysRandom" | "NoEviction" | "VolatileLFU" | "VolatileLRU" | "VolatileRandom" | "VolatileTTL";
   port?: number;
@@ -23,22 +23,21 @@ export function setupRedis(args: {
   const config = new Config();
 
   // Create Redis Enterprise Cluster
-  const redisCluster = new cache.RedisEnterprise(`atscale-${args.env}`, {
+  const redisCluster = new cache.RedisEnterprise(`atscale-redis-${args.env}`, {
     resourceGroupName: args.azureBuilder.resourceGroupName,
     location: args.location,
     sku: {
       name: args.redisConfig.skuName,
+      capacity: args.redisConfig.capacity,
     },
-    minimumTlsVersion: args.redisConfig.minimumTlsVersion ?? "1.2",
-    ...(args.redisConfig.zones && { zones: args.redisConfig.zones }),
-    tags: args.azureBuilder.tags,
+    minimumTlsVersion: "1.2",
   });
 
-  // Create Redis Enterprise Database (must be named "default")
+  // Create Redis Enterprise Database
   const redisDatabase = new cache.Database(`atscale-redis-db-${args.env}`, {
-    databaseName: "default",
     resourceGroupName: args.azureBuilder.resourceGroupName,
     clusterName: redisCluster.name,
+    databaseName: "default",
     clientProtocol: "Encrypted",
     clusteringPolicy: args.redisConfig.clusteringPolicy ?? "EnterpriseCluster",
     evictionPolicy: args.redisConfig.evictionPolicy ?? "NoEviction",
@@ -49,10 +48,9 @@ export function setupRedis(args: {
   const redisKeys = cache.listDatabaseKeysOutput({
     resourceGroupName: args.azureBuilder.resourceGroupName,
     clusterName: redisCluster.name,
-    databaseName: "default",
+    databaseName: redisDatabase.name,
   });
 
-  // Create separate AzureBuilder for private endpoint in shared infra VNet
   const peBuilder = new AzureBuilder({
     app: args.azureBuilder.tags.app,
     env: args.azureBuilder.tags.env,
@@ -75,30 +73,31 @@ export function setupRedis(args: {
         {
           name: "atscale-redis-connection",
           privateLinkServiceId: redisCluster.id,
-          groupIds: ["redisCache"],
+          groupIds: ["redisEnterprise"],
         },
       ],
     }
   );
 
-  // DNS zone group for Redis Enterprise
-  const redisDnsZones = {
-    "redisCache": [
-      { name: "privatelink.redisenterprise.cache.azure.net", id: args.redisPrivateDnsZoneId },
-    ],
-  };
-
-  const redisDnsZoneGroup = peBuilder.Network.PrivateDnsZoneGroup.ForPrivateEndpointCustom(
+  // DNS zone group for Redis Enterprise - using raw resource since builder doesn't recognize Redis Enterprise
+  const redisDnsZoneGroup = new network.PrivateDnsZoneGroup(
     "atscale-redis-dnszonegroup",
-    redisPrivateEndpoint,
-    redisDnsZones
+    {
+      privateDnsZoneGroupName: "default",
+      privateEndpointName: redisPrivateEndpoint.name,
+      resourceGroupName: config.require("vnetRg"),
+      privateDnsZoneConfigs: [
+        {
+          name: "redisenterprise-config",
+          privateDnsZoneId: args.redisPrivateDnsZoneId,
+        },
+      ],
+    }
   );
 
   const redisHost = redisCluster.hostName;
   const redisPort = pulumi.output(args.redisConfig.port ?? 10000);
   const redisPassword = redisKeys.primaryKey;
-
-  const redisConnectionString = pulumi.interpolate`rediss://${redisHost}:${redisPort}/0?password=${redisPassword}`;
 
   return {
     redisCluster,
@@ -107,7 +106,6 @@ export function setupRedis(args: {
     redisHost,
     redisPort,
     redisPassword,
-    redisConnectionString,
     redisPrivateEndpoint,
     redisDnsZoneGroup,
   };
